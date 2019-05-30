d56af90f60c47ff67d517a84a8a4d32f03f494c1
Nate Boyette <nateboyette@icloud.com>
2019-05-30 13:20:21 -0700

    Completed ex1.c

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -1,6 +1,12 @@
 // Write a program that calls `fork()`. Before calling `fork()`, have the main process access a variable
+
 // (e.g., x) and set its value to something (e.g., 100). What value is the variable in the child process?
+
+// -- The value of the variable in the child process is going to be whatever is assigned to it within that process.
+
 // What happens to the variable when both the child and parent change the value of x?
+
+// -- Each process has it's own address and the variable is being assigned a space in memory within that process. So the variable being returned by the child process is a different variable in memory than the parent.
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,7 +14,27 @@
 
 int main(void)
 {
-    // Your code here
+    int x = 100;
+
+    int rc = fork();
+
+    if (rc < 0)
+    {
+        printf("Process Failed");
+        exit(1);
+    }
+    else if (rc == 0)
+    {
+        x = 90;
+        // printf("Child: %d\n", x);
+    }
+    else
+    {
+        // x = 50;
+        // printf("Parent: %d\n", x);
+    }
+
+    printf("%d\n", x);
 
     return 0;
 }
